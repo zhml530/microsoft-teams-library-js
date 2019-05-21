f6403b363637d6eca4a499f2d3bb557d93a3475d
Sachin Gupta <saching@microsoft.com>
2019-05-21 12:29:39 -0700

    preparing release 1.5.0-beta.3

---
+++ dts/private/index.promise.d.ts
@@ -0,0 +1,3 @@
+export { menus } from "./menus";
+export { ChatMembersInformation, FilePreviewParameters, NotificationTypes, ShowNotificationParameters, TeamInstanceParameters, ThreadMember, UserJoinedTeamsInformation, } from "./interfaces";
+export { enterFullscreen, exitFullscreen, getChatMembersAsync as getChatMembers, getConfigSettingAsync as getConfigSetting, getUserJoinedTeamsAsync as getUserJoinedTeams, openFilePreview, sendCustomMessage, showNotification, uploadCustomAppAsync as uploadCustomApp } from "./privateAPIs";
---
+++ dts/public/index.promise.d.ts
@@ -0,0 +1,7 @@
+export { authenticationAsync as authentication } from "./authentication.promise";
+export { HostClientType, TaskModuleDimension, TeamType, UserTeamRole } from "./constants";
+export { Context, DeepLinkParameters, TabInformation, TabInstance, TabInstanceParameters, TaskInfo, TeamInformation } from "./interfaces";
+export { enablePrintCapability, executeDeepLinkAsync as executeDeepLink, getContextAsync as getContext, getMruTabInstancesAsync as getMruTabInstances, getTabInstancesAsync as getTabInstances, initialize, navigateBack, navigateCrossDomain, navigateToTab, print, registerBackButtonHandler, registerBeforeUnloadHandler, registerChangeSettingsHandler, registerFullScreenHandler, registerOnThemeChangeHandler, shareDeepLink } from "./publicAPIs";
+export { settingsAsync as settings } from "./settings.promise";
+export { tasks } from "./tasks";
+export { ChildAppWindow, IAppWindow, ParentAppWindow } from "./appWindow";
